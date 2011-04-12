de7278b298d2a1c2965c6659beb2162a59ce1e11
Tim Fletcher <mail@tfletcher.com>
2011-04-12 09:25:07 +0100

    Add parsing rules for strings, characters, and escape sequences

--- src/rdf_ntriples.erl
+++ src/rdf_ntriples.erl
@@ -56,18 +56,18 @@ pardec_rules() ->
   , {uriref, {tag, uriref, {re, "^<([^>]+)>", capture, 1}}}
   , {nodeid, {tag, nodeid, {unseq, [{skip, "_:"}, {re, "^[A-Za-z][A-Za-z0-9]*"}]}}}
   , {literal, {either, lang_string, datatype_string}}
-  , {lang_string, {tag, plain_literal, {either, lang_string_with_language, lang_string_without_language}}}
-  , {lang_string_with_language, {tuple, [
-      {skip, $"}
-    , {re, "^[^\\\"]*"}
-    , {skip, "\\\"@"}
-    , {re, "^([a-z]+(\\-[-a-z0-9]+)*)"}
-    ]}}
-  , {lang_string_without_language, {re, "^\\\"([^\\\"]*)\\\"", capture, 1}}
-  , {datatype_string, {tag, typed_literal, {tuple, [
-      {re, "^\\\"([^\\\"]*)\\\"", capture, 1}
-    , {skip, "^^"}
-    , uriref
-    ]}}}
+  , {lang_string, {tag, plain_literal, {either, lang_string_with_language, string}}}
+  , {lang_string_with_language, {tuple, [string, {skip, $@}, {re, "^([a-z]+(\\-[a-z0-9]+)*)"}]}}
+  , {datatype_string, {tag, typed_literal, {tuple, [string, {skip, "^^"}, uriref]}}}
   , {ws, {either, 'SP', 'HTAB'}}
+  , {string, {unseq, [{skip, $"}, {'*', {either, escape_sequence, character}, until, $"}, {skip, $"}]}}
+  , {character, {char, {16#20, 16#7E}}}
+  , {escape_sequence, {either, [
+      {map, {re, "^\\\\u([0-9A-F]{4})", capture, 1}, {erlang, list_to_integer, [16]}}
+    , {const, "\\t", $\t}
+    , {const, "\\n", $\n}
+    , {const, "\\r", $\r}
+    , {const, "\\\"", $"}
+    , {const, "\\\\", $\\}
+    ]}}
   ].
